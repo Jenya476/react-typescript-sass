3034abc2428c589f3cf2f66336eb7dfcd7f58575
jenya <jenya476@gmail.com>
2020-01-27 11:14:43 +0200

    added mapDispatchToProps

--- src/app/components/product-card/ProductCard.tsx
+++ src/app/components/product-card/ProductCard.tsx
@@ -1,27 +1,36 @@
 import React, {Component} from 'react';
 import {Product} from '../../models/product.model';
 import './product-card.sass';
-import store from '../../stores/root.store';
-import {addProduct} from '../../actions/cart.action';
+import {connect} from 'react-redux';
+import {bindActionCreators, Dispatch} from 'redux';
+import * as CartActionCreators from '../../actions/cart.action';
 
 interface ProductCardProps {
   product: Product;
 }
-class ProductCard extends Component<ProductCardProps> {
+
+class ProductCard extends Component<any> {
   product: Product;
-  dispatch = store.dispatch;
+
   constructor(props: ProductCardProps) {
     super(props);
     this.product = props.product;
+    console.log('props', props);
   }
+
   render() {
     return (
       <div className="product-card">
         <p className="product-card__title">{this.product.title}</p>
         <p className="product-card__price">{this.product.price} UAH</p>
-        <button onClick={() => this.dispatch(addProduct(this.product))} className="product-card__btn">Add to cart</button>
+        <button onClick={() => this.props.addProduct(this.product)} className="product-card__btn">Add to cart</button>
       </div>);
   }
 }
 
-export default ProductCard
+function mapDispatchToProps(dispatch: Dispatch) {
+  return bindActionCreators(CartActionCreators, dispatch);
+}
+
+// @ts-ignore
+export default connect(undefined, mapDispatchToProps)(ProductCard);
